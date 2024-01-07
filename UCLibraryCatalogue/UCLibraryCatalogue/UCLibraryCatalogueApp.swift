0b07d4556e7c9bb27b34c04bad51aa4e077e0142
Manuel Johan Tito <75228906+mjohantito@users.noreply.github.com>
2024-01-07 10:25:57 +0700

    Add files via upload

---
+++ UCLibraryCatalogue/UCLibraryCatalogue/UCLibraryCatalogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  UCLibraryCatalogueApp.swift
+//  UCLibraryCatalogue
+//
+//  Created by Manuel Johan Tito on 06/01/24.
+//
+
+import SwiftUI
+
+@main
+struct UCLibraryCatalogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CatalogueView()
+        }
+    }
+}
